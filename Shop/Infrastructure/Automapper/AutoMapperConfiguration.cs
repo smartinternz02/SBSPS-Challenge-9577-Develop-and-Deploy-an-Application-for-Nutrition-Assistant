361056e3e514a4a715b8807e9dd865bf2c6a8cb8
ace <94176142+optimisticace2003@users.noreply.github.com>
2022-10-16 19:24:45 +0530

    Shop_file1

---
+++ Shop/Infrastructure/Automapper/AutoMapperConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Shops.BLL.Infrastructure.Automapper;
+
+namespace Shop.Infrastructure.Automapper
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DtoToEntityProfile>();
+                x.AddProfile<EntityToDtoProfile>();
+            });
+        }
+    }
+}
